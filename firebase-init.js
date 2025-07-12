fb2be7c50fdcd8546b9067fff6891490ea0092e5
rmoharana038 <85046291+rmoharana038@users.noreply.github.com>
2025-07-12 21:02:03 +0530

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,7 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
