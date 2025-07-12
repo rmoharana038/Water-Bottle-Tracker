8088682e86824276bff574f3402214d57e7ce2c9
rmoharana038 <85046291+rmoharana038@users.noreply.github.com>
2025-07-12 23:57:11 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,4 +1,3 @@
-// firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
