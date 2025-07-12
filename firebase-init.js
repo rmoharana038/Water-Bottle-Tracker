b3862424c6ffcea7d1754b0d5edbcd5f10687145
rmoharana038 <85046291+rmoharana038@users.noreply.github.com>
2025-07-12 21:44:24 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,7 +1,15 @@
 // firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { firebaseConfig } from "./firebase-config.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpCzMa8MkWPi9-5oj0O6q-eCbZ9nxmzms",
+  authDomain: "water-bottle-tracker-43537.firebaseapp.com",
+  projectId: "water-bottle-tracker-43537",
+  storageBucket: "water-bottle-tracker-43537.appspot.com",
+  messagingSenderId: "424777349690",
+  appId: "1:424777349690:web:54056417c24cd2f0329303"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
