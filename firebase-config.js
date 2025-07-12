7a4733bda53d851052071e490ba0637e1fda82fc
rmoharana038 <85046291+rmoharana038@users.noreply.github.com>
2025-07-12 22:56:44 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,3 +7,12 @@ export const firebaseConfig = {
   messagingSenderId: "424777349690",
   appId: "1:424777349690:web:54056417c24cd2f0329303"
 };
+import { enableIndexedDbPersistence } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
+
+enableIndexedDbPersistence(db).catch((err) => {
+  if (err.code === 'failed-precondition') {
+    console.warn("Persistence can only be enabled in one tab at a time.");
+  } else if (err.code === 'unimplemented') {
+    console.warn("This browser doesn't support offline persistence.");
+  }
+});
