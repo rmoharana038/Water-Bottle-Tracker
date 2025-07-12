5b0b23c28f1a5205e6a4ef8b529dfff08d7c7345
rmoharana038 <85046291+rmoharana038@users.noreply.github.com>
2025-07-12 21:01:34 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,27 +1,9 @@
 // firebase-config.js
-
-// Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
-
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDpCzMa8MkWPi9-5oj0O6q-eCbZ9nxmzms",
+export const firebaseConfig = {
+  apiKey: "AlzaSyDpCzMa8MkWPi9-5oj006q-cCbZ9nxmzms",
   authDomain: "water-bottle-tracker-43537.firebaseapp.com",
   projectId: "water-bottle-tracker-43537",
   storageBucket: "water-bottle-tracker-43537.appspot.com",
   messagingSenderId: "424777349690",
-  appId: "1:424777349690:web:54056417c24cd2f0329303",
-  measurementId: "G-ZKGP7LC80E"
+  appId: "1:424777349690:web:5405641f7c24cd2f032930"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
-
-// Export Firebase services for use in other files
-export { auth, db, analytics };
