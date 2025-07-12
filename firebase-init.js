1ae4a46b77c4a3d016410c42e7bb98de438fdda8
rmoharana038 <85046291+rmoharana038@users.noreply.github.com>
2025-07-12 22:50:29 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,7 @@
 // firebase-init.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDpCzMa8MkWPi9-5oj0O6q-eCbZ9nxmzms",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
