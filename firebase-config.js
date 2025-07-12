f4b34b666483ac85cac2e8ea83732fc47d980022
rmoharana038 <85046291+rmoharana038@users.noreply.github.com>
2025-07-12 21:09:51 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // firebase-config.js
 export const firebaseConfig = {
-  apiKey: "AlzaSyDpCzMa8MkWPi9-5oj006q-cCbZ9nxmzms",
+  apiKey: "AIzaSyDpCzMa8MkWPi9-5oj0O6q-eCbZ9nxmzms",
   authDomain: "water-bottle-tracker-43537.firebaseapp.com",
   projectId: "water-bottle-tracker-43537",
   storageBucket: "water-bottle-tracker-43537.appspot.com",
   messagingSenderId: "424777349690",
-  appId: "1:424777349690:web:5405641f7c24cd2f032930"
+  appId: "1:424777349690:web:54056417c24cd2f0329303"
 };
